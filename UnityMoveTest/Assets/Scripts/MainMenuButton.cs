0a4e14c66ae53598b1ae14d201418f656d391b8c
Fer <ferraniusd@gmail.com>
2019-06-03 18:07:26 +0200

    Añadido control de la escena principal con cursor (el mando)

---
+++ UnityMoveTest/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public void goToScene()
+    {
+        if (this.gameObject.name == "2 Players")
+        {
+            SceneManager.LoadScene("SableLaserPrototipoP2", LoadSceneMode.Single);
+        }
+        else if(this.gameObject.name == "1 Player")
+        {
+            SceneManager.LoadScene("SableLaserPrototipoP1", LoadSceneMode.Single);
+        }
+    }
+}
