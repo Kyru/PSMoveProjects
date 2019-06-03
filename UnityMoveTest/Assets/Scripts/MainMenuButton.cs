952afa305fae0b395b809d4c257aad6f4056794d
Fer <ferraniusd@gmail.com>
2019-06-03 19:26:11 +0200

    Se cambia el color del botón al clicarlo

--- UnityMoveTest/Assets/Scripts/MainMenuButton.cs
+++ UnityMoveTest/Assets/Scripts/MainMenuButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButton : MonoBehaviour
@@ -9,10 +10,12 @@ public class MainMenuButton : MonoBehaviour
     {
         if (this.gameObject.name == "2 Players")
         {
+            this.gameObject.GetComponent<Image>().color = new Color(50, 50, 50, 50);
             SceneManager.LoadScene("SableLaserPrototipoP2", LoadSceneMode.Single);
         }
         else if(this.gameObject.name == "1 Player")
         {
+            this.gameObject.GetComponent<Image>().color = new Color(50, 50, 50, 50);
             SceneManager.LoadScene("SableLaserPrototipoP1", LoadSceneMode.Single);
         }
     }
