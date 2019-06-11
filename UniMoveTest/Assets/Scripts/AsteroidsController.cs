7f5af7a36e718f76776616fea0eea5333f86ec81
Fer <ferraniusd@gmail.com>
2019-06-11 17:48:17 +0200

    Mapa lleno de asteroides y script de asignar tag (Asteroid)

---
+++ UniMoveTest/Assets/Scripts/AsteroidsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsController : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (Transform t in transform)
+        {
+            t.gameObject.tag = "Asteroid";
+        }
+    }
+}
