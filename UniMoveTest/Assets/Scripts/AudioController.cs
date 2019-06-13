fe9f619be2d0eab0a0c1358eb9997eb544fb80e9
Fer <ferraniusd@gmail.com>
2019-06-13 22:49:21 +0200

    Sound FX stopped when gameOver

--- UniMoveTest/Assets/Scripts/AudioController.cs
+++ UniMoveTest/Assets/Scripts/AudioController.cs
@@ -6,16 +6,28 @@ public class AudioController : MonoBehaviour
 {
     public AudioClip[] audioClips;
     private AudioSource audioSource;
+    bool isGameOver;
+
+    void Awake(){
+        Messenger.AddListener(GameEvent.GAME_OVER, gameOver);
+    }
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        isGameOver = false;
     }
 
     public void PlayAudioClip(int clipToPlay)
     {
         audioSource.clip = audioClips[clipToPlay];
         audioSource.Play();
+
+        if(isGameOver) Invoke("DisableAudioSource", 2f);
+    }
+
+    void DisableAudioSource(){
+        audioSource.enabled = false;
     }
 
     public void SetLoop(bool isActive)
@@ -28,4 +40,11 @@ public class AudioController : MonoBehaviour
         return audioSource.loop;
     }
 
+    void gameOver(){
+        isGameOver = true;
+    }
+
+    void OnDestroy(){
+        Messenger.RemoveListener(GameEvent.GAME_OVER, gameOver);
+    }
 }
