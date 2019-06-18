0e55677a64ed67e4d2f9baa5d9f4093e90b8abfb
Fer <ferraniusd@gmail.com>
2019-06-18 21:44:03 +0200

    Added Decoration to Lightcubes scene P1

---
+++ UnityMoveTest/Assets/Scripts/AutoRotator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotator : MonoBehaviour
+{
+    private int randRotationType;
+    private int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 50;
+        randRotationType = Random.Range(0, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (randRotationType == 0)
+        {
+            transform.Rotate(Vector3.right * speed * Time.deltaTime);
+        }
+        else if (randRotationType == 1)
+        {
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
